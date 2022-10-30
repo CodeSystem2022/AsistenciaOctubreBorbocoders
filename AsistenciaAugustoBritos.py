52bab9fe0ac41eea1e2291110f8c5fc4b2d0a1c2
Augusto Britos <augustobritos95@gmail.com>
2022-10-30 16:54:53 -0300

    AsistenciaAugustoBritos

---
+++ AsistenciaAugustoBritos.py
@@ -0,0 +1,17 @@
+class Cubo:
+
+    def __init__(self, ancho, altura, profundidad):
+        self.ancho = ancho
+        self.altura = altura
+        self.profundidad = profundidad
+
+    def calcular_volumen(self):
+        return self.ancho * self.altura * self.profundidad
+
+
+ancho = int(input('Ingrese el ancho: '))
+altura = int(input('Ingrese la altura: '))
+profundidad = int(input('Ingrese la profundidad: '))
+
+cubo1 = Cubo(ancho, altura, profundidad)
+print(f'El volumen es: {cubo1.calcular_volumen()}')
